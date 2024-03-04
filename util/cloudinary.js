eb1bcc9ac109686c6201042a421c0f0066d4ee4a
mrafeeqvr <muhammedrafeeq805@gmail.com>
2024-03-04 08:54:24 +0530

    addedd util files

---
+++ util/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary');
+
+cloudinary.config({
+  cloud_name: 'dra4o4yvc',
+  api_key: '442954646228255',
+  api_secret: 'Q1tglbOqt0yzwhv5bXM6DG6L8uY'
+});
+
+module.exports = cloudinary;
